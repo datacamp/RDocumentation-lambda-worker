5ac0b9eacc6b29164de2d1365d2ce3f99bf9c018
Ludovic Vannoorenberghe <ludovic.v@me.com>
2016-05-10 13:58:26 +0200

    unzip function

--- functions/upload-packages/index.js
+++ functions/upload-packages/index.js
@@ -1,12 +1,19 @@
 var AWS = require('aws-sdk'); 
 
+var JSFtp = require('jsftp');
+
 exports.handler = function(e, ctx) {
   var s3 = new AWS.S3();
+  var Ftp = new JSFtp({
+    host: 'ftp://cran.r-project.org/'
+  });
 
   var params = {
     Bucket: 'blog.datacamp.com', 
     EncodingType: 'url'
   };
+
+
 
   s3.listObjects(params, function(err, data) {
     if (err) {
