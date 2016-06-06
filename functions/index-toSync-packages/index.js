99aa8e873cbbe6afded039c6ae23ae60e12838c7
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-06 19:30:01 +0200

    automate package processing

--- functions/index-toSync-packages/index.js
+++ functions/index-toSync-packages/index.js
@@ -1,27 +1,52 @@
 var AWS = require('aws-sdk'); 
 var Promise = require('bluebird');
-var fs = require('fs');
 
-var listToSyncPackageVersions = function(dynDB, lastKey, callback) {
+var listToSyncPackageVersions = function(dynDB, lastKey, limit, callback) {
   console.info('Fetching package list');
   var params = {
     TableName: 'rdoc-packages',
-    FilterExpression: 'attribute_not_exists(SyncResult)',
+    FilterExpression: 'SyncResult <> :success',
+    ExpressionAttributeValues: {
+      ':success': 200
+    },
+    Limit: limit,
     ProjectionExpression: 'PackageName, PackageVersion',
     ExclusiveStartKey: lastKey
   };
   return dynDB.scan(params, callback);
 };
 
+var getObject = function(s3, bucket, key, cb) {
+  var params = {
+    Bucket: bucket, /* required */
+    Key: key, /* required */
+    ResponseContentType: 'application/json'
+  };
+  s3.getObject(params, cb);
+};
+
+var putObject = function(s3, key, body, cb) {
+  s3.putObject({
+    Bucket: 'assets.rdocumentation.org', 
+    Key: key,
+    Body: body
+  }, cb);
+};
+
+
 
 exports.handle = function(e, ctx) {
   var dynamodb = new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'});
+  var s3 = new AWS.S3();
 
   var buffer = [];
   var bufferId = 0;
+  var bucketName = 'assets.rdocumentation.org';
+  var lastEvaluatedKeyS3Key = 'rpackages/lastKey.json';
+  var newLastKey = null;
 
-  var getToSyncPackages = function(list, lastKey) {
-    return Promise.promisify(listToSyncPackageVersions)(dynamodb, lastKey)
+  var getToSyncPackages = function(list, lastKey, limit) {
+    return Promise.promisify(listToSyncPackageVersions)(dynamodb, lastKey, limit)
       .then(function(packageList) {
         console.log(packageList.Count);
         var mappedList = packageList.Items.map(function(packageVersion) {
@@ -32,40 +57,54 @@ exports.handle = function(e, ctx) {
             version: version
           };
         });
-        if (packageList.LastEvaluatedKey) {
-          return getToSyncPackages(mappedList, packageList.LastEvaluatedKey);
+        newLastKey = packageList.LastEvaluatedKey;
+        if (list.length + mappedList.length < limit) {
+          return getToSyncPackages(list.concat(mappedList), packageList.LastEvaluatedKey, limit);
         }
         else return list.concat(mappedList);
       });
   };
   
-  getToSyncPackages([], null)
+  Promise.promisify(getObject)(s3, bucketName, lastEvaluatedKeyS3Key)
+    .then(function(s3Object) {
+      try {
+        return JSON.parse(s3Object.Body);
+      } catch(err) {
+        return null;
+      }
+    })
+    .then(function(lastKey) {
+      return getToSyncPackages([], lastKey, 500);
+    })
+    .then(function(list) {
+      console.log(newLastKey);
+      return Promise.promisify(putObject)(s3, lastEvaluatedKeyS3Key, JSON.stringify(newLastKey, null, 2))
+      .then(function() {
+        return list;
+      });
+    })
     .each(function(packageVersion) {
       buffer.push(packageVersion);
       if (buffer.length >= 100) {
         var json = JSON.stringify(buffer, null, 2);
-        return Promise.promisify(fs.open)('toSync/toSync' + bufferId + '.json', 'w')
-          .then(function(fd) {
-            return Promise.promisify(fs.writeFile)(fd, json);
-          })
-          .then(function(res) {
+        return Promise.promisify(putObject)(s3, 'rpackages/toSync/toSync' + bufferId + '.json', json)
+          .then(function() {
             bufferId++;
             buffer = [];
             console.info('Wrote buffer');
+            return 0;
           });
-      }
+      } else return 1;
     })
     .then(function() {
       var json = JSON.stringify(buffer, null, 2);
-      return Promise.promisify(fs.open)('toSync/toSync' + bufferId + '.json', 'w')
-        .then(function(fd) {
-          return Promise.promisify(fs.writeFile)(fd, json);
-        })
-        .then(function(res) {
-          bufferId++;
-          buffer = [];
-          console.info('Wrote buffer');
-        });
+        return Promise.promisify(putObject)(s3, 'rpackages/toSync/toSync' + bufferId + '.json', json)
+          .then(function() {
+            bufferId++;
+            buffer = [];
+            console.info('Wrote buffer');
+            return 0;
+          });
     })
     .then(function() {
       ctx.succeed();
