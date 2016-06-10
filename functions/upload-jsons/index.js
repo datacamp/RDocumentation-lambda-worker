302011c01fde2c35b5c4b02d691eda8dacd781a7
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-10 10:54:28 +0200

    increase concurrency

--- functions/upload-jsons/index.js
+++ functions/upload-jsons/index.js
@@ -130,7 +130,7 @@ exports.handle = function(e, ctx) {
         .catch(function(error) {
           return error;
         });
-    }, {concurrency: 3})
+    }, {concurrency: 5})
     .then(function() {
       ctx.succeed();
     })
