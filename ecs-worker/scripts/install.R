60f41e2aab0f2cec225b0784a1b4542d3fd4a200
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-07-19 13:50:31 +0200

    new dependency

--- ecs-worker/scripts/install.R
+++ ecs-worker/scripts/install.R
@@ -1,3 +1,3 @@
-install.packages("aws.sqs", repos = c(getOption("repos"), "http://cloudyr.github.io/drat"))
 install.packages("devtools")
+install.packages("aws.sqs", repos = c(getOption("repos"), "http://cloudyr.github.io/drat"))
 devtools::install_github("datacamp/staticdocs")
